c381575d9c13feb3b02044d91a9b39930c020573
William Short <darkhelmet125@csu.fullerton.edu>
2014-11-01 17:26:10 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Seal.m
@@ -0,0 +1,23 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by William Short on 11/1/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+
+-(id)init {
+    self = [super init];
+    
+    if (self) {
+        CCLOG(@"Seal Created");
+    }
+    
+    return self;
+}
+
+@end
