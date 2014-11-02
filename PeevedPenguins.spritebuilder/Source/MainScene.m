23dde9a120b13e9c3d3d1fd1b77a885e12841fc7
William Short <darkhelmet125@csu.fullerton.edu>
2014-11-01 18:19:48 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/MainScene.m
+++ PeevedPenguins.spritebuilder/Source/MainScene.m
@@ -13,6 +13,8 @@
 -(void)play {
     
     CCLOG(@"Play button pressed");
+    CCScene *gameplayScene = [CCBReader loadAsScene:@"Gameplay"];
+    [[CCDirector sharedDirector] replaceScene:gameplayScene];
     
 }
 
